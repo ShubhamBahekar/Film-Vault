ce86bea7e0e31a80daaf9642f90e954a975437ce
ShubhamBahekar <shubhambahekar02@gmail.com>
2025-09-12 19:21:36 +0530

    removed bugs

--- film-vault/src/config/apiConstants.js
+++
@@ -1,5 +0,0 @@
-export const API_BASE_URL = "https://api.imdbapi.dev";
-
-export const API_METHODS = {
-  GET: "GET",
-};
--- film-vault/src/services/authServices.jsx
+++
@@ -1,4 +0,0 @@
-import { getApi} from "./api";
-
-export const getMoviesByTitle = async() =>getApi("/titles") ;
-
